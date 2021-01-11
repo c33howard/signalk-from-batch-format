d819b155846bf8d6ed7cd388c14f3a966541bc38
Craig Howard <craig@choward.ca>
2021-01-10 21:06:13 -0800

    Update to latest signalk-batcher
    Refactor reading the batch into signalk-batcher, where it's properly
    unit tested, etc.

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const trace = require('debug')('signalk-from-batch-format:trace');
 
 const _ = require('lodash');
 const aws = require('aws-sdk');
+const from_batch = require('signalk-batcher').from_batch_to_delta;
 const zlib = require('zlib');
 
 const s3 = new aws.S3();
@@ -30,9 +31,9 @@ module.exports = function(app) {
 
     let _process_message = async function(body) {
         const s3_result = _get_from_s3(body);
-        const rows = await _parse_json(s3_result);
+        const batch = await _parse_json(s3_result);
 
-        _publish_to_signalk(rows);
+        _publish_to_signalk(batch);
     };
 
     let _get_from_s3 = function(body) {
@@ -69,45 +70,9 @@ module.exports = function(app) {
         }
     };
 
-    // TODO: refactor into signalk-batcher and add unit tests
-    let _visit_non_objects = function(obj, fn) {
-        let _do_visit = function(obj, path, fn) {
-            _.forIn(obj, function(value, key) {
-                const key_path = path != '' ? `${path}.${key}`: key;
-
-                // if value is an object, recurse (note that arrays are
-                // objects, so hence the plain object check)
-                if (_.isPlainObject(value)) {
-                    _do_visit(value, key_path, fn);
-                }
-                // otherwise, emit the parent
-                else {
-                    fn(path, obj);
-                }
-            });
-        };
-
-        return _do_visit(obj, '', fn);
-    };
-
-    let _publish_to_signalk = function(state) {
-        const context_self = `vessels.${state.self}`;
-        const base_time_ms = Date.parse(state.timestamp);
-
-        const vessel_state = _.get(state, context_self);
-
-        // list of complex objects that need to be fixed, due to how signalk
-        // for some reasons allows complex objects for some data types
-        const complex_objects = [
-            { re: /\.position\.(latitude|longitude)$/, num: 2 },
-            { re: /\.attitude\.(yaw|roll|pitch)$/, num: 3 },
-        ];
-
-        // a cache of what we've seen so far for the complex objects
-        const complex_object_cache = {};
-
-        // helper function to do the actual publishing
-        const publish = function($source, last_delta_t, path, value) {
+    let _publish_to_signalk = function(batch) {
+        const deltas = from_batch(batch);
+        deltas.forEach(function(delta) {
             // TODO: it'd be better to build up a batch and send a single
             // delta, but since handleMessage overrides the source.label
             // attribute, I have to pretend to be multiple providers
@@ -116,75 +81,8 @@ module.exports = function(app) {
             //
             // The real TODO is to fix signalk itself, once I understand the
             // reasoning as to why it works this way.
-            const msg = {
-                context: context_self,
-                updates: [{
-                    $source: $source,
-                    timestamp: new Date(base_time_ms + last_delta_t),
-                    values: [{
-                        path: path,
-                        value: value
-                    }]
-                }]
-            };
-
-            app.handleMessage($source, msg);
-        };
-
-        // TODO: refactor into signalk-batcher and add unit tests
-        _visit_non_objects(vessel_state, function(path, value) {
-            // path is where we are in the model
-            // value is an array with source to list elements, ie:
-            //
-            //  {
-            //      test-source-1: [[0, 1], [1, 1.2], [2, 1]],
-            //      test-source-2: [[0, 1], [1], [2, 1.1]]
-            //  }
-            //
-            // Each key is a source reference (ie $source), the first element
-            // is the ms from base_time_ms, and the second is the value.  If
-            // value is ommitted, it's the same as the previous value.
-
-            // for each of the sources, publish
-            _.forIn(value, function(points, $source) {
-                // skip to the end and publish the last time with the last
-                // unique value
-                const last_delta_t = _.last(points)[0];
-                // note: might have to fix this if we're dealing with a complex object
-                let last_value = _.findLast(points, function(pair) {
-                    return pair.length == 2;
-                })[1];
-
-                // check against the complex objects to see if this path is one
-                // of the ones that needs to be fixed
-                const complex_obj = _.find(complex_objects, function(co) {
-                    return co.re.test(path);
-                });
-
-                // if we found a complex object, adjust the path and value
-                if (complex_obj) {
-                    // 1.take the last element off the path
-                    const path_prefix = path.substring(0, path.lastIndexOf('.'));
-                    const path_suffix = path.substring(path.lastIndexOf('.') + 1);
-
-                    // 2. store what we've found so far
-                    _.defaults(complex_object_cache, { [path_prefix]: {} });
-                    _.merge(complex_object_cache[path_prefix], {
-                        [path_suffix]: last_value
-                    });
-
-                    // 3. if we now have all the elements, reconstruct the object
-                    // and handle the message
-                    if (_.keys(complex_object_cache[path_prefix]).length === complex_obj.num) {
-                        trace(`fixed complex object ${path_prefix}`);
-                        path = path_prefix;
-                        last_value = complex_object_cache[path_prefix];
-                    }
-                }
-
-                // publish
-                publish($source, last_delta_t, path, last_value);
-            });
+            const $source = delta.updates.$source;
+            app.handleMessage($source, delta);
         });
     };
 
